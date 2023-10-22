808620bee9b3ce37a7db456a898aab75d7bacf2e
Nome <almeidagabriel.nunes@gmail.com>
2023-10-22 19:40:27 -0300

    Arquivo para conectar no banco de dados, criar um caso ainda n exista e criiar tabela caso n tenha

--- conexao.php
+++ conexao.php
@@ -2,5 +2,35 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = 
+$dbname = "produtobd";
+
+
+try{
+    $conn = new PDO("mysql:host$servername", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Banco Conectado";
+
+    //Criando o banco de dados caso ele n exista
+    $exists = $conn->prepare("CREATE DATABASE IF NOT EXISTS produtobd;");
+    $exists-> execute();
+
+
+    //adicionar bando de dados à conexão
+    $conn = new PDO("mysql:host$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    //criando as tabelas caso elas não existam 
+    if ($exists){
+        $createTableUsuario = $conn->prepare("CREATE TABLE  IF NOT EXISTS USUARIO(
+            cd_usuario INTEGER PRIMARY KEY auto_incriment,
+            nm_usuario varchar(50) NOT NULL,
+            nm_email varchar(100) NOT NULL,
+            cd_senha varchar(250) NOT NULL,
+            CONSTRAINT un_email unique(nm_email)
+            );");
+    }
+}catch(PDOException $e){
+    echo "Connection Failed: " . $e->getMessage(); 
+}
+
 ?>
