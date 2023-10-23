547de1231f2506e16a5a1dffa651f37c666d5d4a
Nome <almeidagabriel.nunes@gmail.com>
2023-10-22 21:49:14 -0300

    Erros de digitacao

--- conexao.php
+++ conexao.php
@@ -4,35 +4,27 @@ $username = "root";
 $password = "";
 $dbname = "produtobd";
 
-
-try{
-    $conn = new PDO("mysql:host$servername", $username, $password);
+try {
+    $conn = new PDO("mysql:host=$servername", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Banco Conectado";
 
-    //Criando o banco de dados caso ele n exista
-    $exists = $conn->prepare("CREATE DATABASE IF NOT EXISTS produtobd;");
-    $exists-> execute();
+    // Criando o banco de dados caso ele não exista
+    $conn->exec("CREATE DATABASE IF NOT EXISTS $dbname");
 
-
-    //adicionar bando de dados à conexão
-    $conn = new PDO("mysql:host$servername;dbname=$dbname", $username, $password);
+    // Conectando ao banco de dados criado
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    //criando as tabelas caso elas não existam 
-    if ($exists){
-        $createTableUsuario = $conn->prepare("CREATE TABLE  IF NOT EXISTS USUARIO(
-            cd_usuario INTEGER PRIMARY KEY auto_incriment,
-            nm_usuario varchar(50) NOT NULL,
-            nm_email varchar(100) NOT NULL,
-            cd_senha varchar(250) NOT NULL,
-            CONSTRAINT un_email unique(nm_email)
-            );");
-    }
-
-
-}catch(PDOException $e){
-    echo "Connection Failed: " . $e->getMessage(); 
+    // Criando a tabela de usuário caso ela não exista
+    $createTableUsuario = $conn->exec("CREATE TABLE IF NOT EXISTS USUARIO (
+        cd_usuario INTEGER PRIMARY KEY AUTO_INCREMENT,
+        nm_usuario VARCHAR(50) NOT NULL,
+        nm_email VARCHAR(100) NOT NULL,
+        cd_senha VARCHAR(250) NOT NULL,
+        CONSTRAINT un_email UNIQUE(nm_email)
+    );");
+} catch (PDOException $e) {
+    echo "Connection Failed: " . $e->getMessage();
 }
-
 ?>
